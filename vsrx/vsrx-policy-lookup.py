040487fd3d1c36f8262fa22ebbb9499d4a3038ba
jeremys <60401781+js087@users.noreply.github.com>
2022-01-06 16:33:40 -0700

    Update vsrx-policy-lookup.py
    Adding protocols and list in 'for' loop - may be useful in the future to cover bases.

--- vsrx/vsrx-policy-lookup.py
+++ vsrx/vsrx-policy-lookup.py
@@ -10,7 +10,7 @@ ip_src = ['10.0.0.1']
 ip_dst = ['10.1.0.1']
 
 #network protocol ports
-port_protocol = 'tcp' #udp
+port_protocol = ['tcp'] #udp, can be a list
 port_src = 1 #almost always source from port 1, rarely do you filter on source ports
 port_dst = [514, 22, 23] #integers, e.g. 22, 25, 80, 443
 
@@ -20,10 +20,11 @@ for zone_s in zone_src:
         for ip_s in ip_src:
             for ip_d in ip_dst:
                 for port_d in port_dst:
-                    print(f"show security match-policies global from-zone {zone_s} to-zone {zone_d} source-ip {ip_s} destination-ip {ip_d} source-port {port_src} destination-port {port_d} protocol {port_protocol} | match action-type")
-                    #for dst to src, inversion of original requests
-                    print(f"show security match-policies global from-zone {zone_d} to-zone {zone_s} source-ip {ip_d} destination-ip {ip_s} source-port {port_src} destination-port {port_d} protocol {port_protocol} | match action-type")
-                    print() #empty return
+                    for port_p in port_protocol:
+                        print(f"show security match-policies global from-zone {zone_s} to-zone {zone_d} source-ip {ip_s} destination-ip {ip_d} source-port {port_src} destination-port {port_d} protocol {port_p} | match action-type")
+                        #for dst to src, inversion of original requests
+                        print(f"show security match-policies global from-zone {zone_d} to-zone {zone_s} source-ip {ip_d} destination-ip {ip_s} source-port {port_src} destination-port {port_d} protocol {port_p} | match action-type")
+                        print() #empty return
 
 #timestamping
 print(f"***** Generated: {datetime.datetime.now()} ***** ")
